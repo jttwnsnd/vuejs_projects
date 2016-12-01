7c56164a2d6578590d6267be2fce59690071cb19
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-01 17:27:16 -0500

    Lesson 16, exercise 1 completed

--- lessons/scripts.js
+++ lessons/scripts.js
@@ -1,6 +1,13 @@
 new Vue({
-  el: "#app",
+  el: "#exercise",
   data: {
-    title: "Hello World"
+    name: "JT Townsend",
+    age: 27,
+    image: 'https://i.kinja-img.com/gawker-media/image/upload/s--zgzpDfnC--/c_scale,fl_progressive,q_80,w_800/ebaeet1udzknntbtepj6.jpg'
+  },
+  methods: {
+    randomInt: function(){
+      return Math.round(Math.random());
+    }
   }
 })
