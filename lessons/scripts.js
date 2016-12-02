fd98a5568fc4760ab6bf1afbcbd3a611192b04a1
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-02 10:36:42 -0500

    Prep for exercise 2

--- lessons/scripts.js
+++ lessons/scripts.js
@@ -18,20 +18,33 @@
 //     }
 //   }
 // })
+
+
+// Lessons on events
+// new Vue({
+//   el: "#app",
+//   data: {
+//     counter: 0,
+//     x: 0,
+//     y: 0
+//   },
+//   methods: {
+//     increase: function(step, event) {
+//       this.counter += step;
+//     },
+//     updateCoordinates: function(event) {
+//       this.x = event.clientX;
+//       this.y = event.clientY;
+//     },
+//     alertMe: function() {
+//       alert('Alert!')
+//     }
+//   }
+// })
+
 new Vue({
-  el: "#app",
+  el: "#exercise2",
   data: {
-    counter: 0,
-    x: 0,
-    y: 0
-  },
-  methods: {
-    increase: function(step, event) {
-      this.counter += step;
-    },
-    updateCoordinates: function(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    }
+    value: ''
   }
 })
