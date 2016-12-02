4260cae17ea390ee5325f691fc62e1f9b3749fd7
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-02 10:24:00 -0500

    event listeners

--- lessons/scripts.js
+++ lessons/scripts.js
@@ -1,19 +1,37 @@
+// first lesson/exercise Vue
+// new Vue({
+//   el: "#exercise",
+//   data: {
+//     name: "JT Townsend",
+//     age: 27,
+//     image: 'https://i.kinja-img.com/gawker-media/image/upload/s--zgzpDfnC--/c_scale,fl_progressive,q_80,w_800/ebaeet1udzknntbtepj6.jpg'
+//   },
+//   methods: {
+//     randomInt: function(){
+//       return Math.random();
+//     },
+//     newName: function(event){
+//       this.name = event.target.value;
+//     },
+//     newAge: function(event){
+//       this.age = event.target.value;
+//     }
+//   }
+// })
 new Vue({
-  el: "#exercise",
+  el: "#app",
   data: {
-    name: "JT Townsend",
-    age: 27,
-    image: 'https://i.kinja-img.com/gawker-media/image/upload/s--zgzpDfnC--/c_scale,fl_progressive,q_80,w_800/ebaeet1udzknntbtepj6.jpg'
+    counter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
-    randomInt: function(){
-      return Math.round(Math.random());
+    increase: function(step, event) {
+      this.counter += step;
     },
-    newName: function(event){
-      this.name = event.target.value;
-    },
-    newAge: function(event){
-      this.age = event.target.value;
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
     }
   }
 })
