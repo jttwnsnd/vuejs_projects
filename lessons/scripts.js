99250d3798ed3115fbbf71305b76bcadaf479aec
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-05 13:52:50 -0500

    before exercise 4

--- lessons/scripts.js
+++ lessons/scripts.js
@@ -117,6 +117,15 @@
 new Vue({
   el: "#app",
   data: {
-    color: 'gray'
+    color: 'gray',
+    width: 100
+  },
+  computed: {
+    myStyle: function() {
+      return {
+        backgroundColor: this.color,
+        width: this.width + 'px'
+      }
+    }
   }
 })
