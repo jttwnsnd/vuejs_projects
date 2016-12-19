6f94a458dc6f6dde9bd0f0a80d02294e34f82181
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-19 13:18:57 -0500

    Sibling compononets talk to each other

--- lessons/module_8/lessons/src/components/UserDetail.vue
+++ lessons/module_8/lessons/src/components/UserDetail.vue
@@ -2,10 +2,28 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+  export default{
+    props: {
+      name: {
+        type: String
+      }
+    },
+    methods: {
+      switchName() {
+        return this.name.split('').reverse().join('');
+      },
+      resetName() {
+        this.name = 'Max';
+        this.$emit('nameWasReset', this.name);
+      }
+    }
+  }
 </script>
 
 <style scoped>
