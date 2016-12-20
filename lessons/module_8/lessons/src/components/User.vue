90001d9b7f15ee0e8262bdf3a7ebff9b7b31cc3b
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-20 10:10:17 -0500

    exercise 1, module 8

--- lessons/module_8/lessons/src/components/User.vue
+++ lessons/module_8/lessons/src/components/User.vue
@@ -6,10 +6,16 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :name="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail
+                :name="name"
+                @nameWasReset="name = $event"
+                :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit
+                  :userAge="age"
+                  @ageWasEdited="age = $event"
+                  ></app-user-edit>
             </div>
         </div>
     </div>
@@ -22,7 +28,8 @@
     export default {
         data: function(){
           return{
-            name: 'Max'
+            name: 'Max',
+            age: 27
           }
         },
         components: {
--- lessons/module_8/lessons/src/components/UserDetail.vue
+++ lessons/module_8/lessons/src/components/UserDetail.vue
@@ -3,16 +3,20 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>user Age: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+  import { eventBus } from '../main'
+
   export default{
     props: {
       name: {
         type: String
-      }
+      },
+      userAge: Number
     },
     methods: {
       switchName() {
@@ -22,6 +26,11 @@
         this.name = 'Max';
         this.$emit('nameWasReset', this.name);
       }
+    },
+    created() {
+      eventBus.$on('ageWasEdited', (age) => {
+         this.userAge = age;
+      })
     }
   }
 </script>
