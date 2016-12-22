3b56e313a12933fd24b9e6eca7d5722ff01ecf06
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-22 11:03:33 -0500

    advanced component work

---
+++ lessons/module_9/Section Code (Start)/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <app-quote>
+                <h1>{{ quoteTitle }}</h1>
+                <p>A wonderful quote!</p>
+              </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from "./components/Quote.vue"
+
+    export default {
+        components: {
+           appQuote: Quote
+        },
+        data: function(){
+          return {
+            quoteTitle: 'The Quote'
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ lessons/module_9/Section Code (Start)/src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  h1 {
+    color: red;
+  }
+
+</style>
