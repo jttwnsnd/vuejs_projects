138fecad36ae8ef725f5154a0b4b5225421aaf6f
jttwnsnd <jt.a.townsend@gmail.com>
2016-12-31 13:06:21 -0500

    building youtube clone list

--- projects/module_9/Start/src/App.vue
+++ projects/module_9/Start/src/App.vue
@@ -3,13 +3,20 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="loadedTemplate = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="loadedTemplate = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="loadedTemplate = 'appRed'">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
+                <keep-alive>
+                  <component :is="loadedTemplate">
+                    <h2 slot="title">{{ inputedTitle }}</h2>
+                    <p slot="content">{{ inputedContent }}</p>
+                  </component>
+                </keep-alive>
+                <hr>
+                <!-- <app-blue></app-blue>
                 <app-green></app-green>
-                <app-red></app-red>
+                <app-red></app-red> -->
             </div>
         </div>
     </div>
@@ -25,6 +32,13 @@
             appBlue: Blue,
             appGreen: Green,
             appRed: Red
+        },
+        data: function(){
+          return{
+            inputedTitle: 'I am a title',
+            inputedContent: 'I am content',
+            loadedTemplate: 'appBlue'
+          }
         }
     }
 </script>
