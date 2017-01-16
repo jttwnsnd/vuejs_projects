e52faf1c4d4ca74bb5fec65035cacb25c89f1bb0
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-16 09:34:13 -0500

    got quote to view

---
+++ projects/module_10/Start/src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="row">
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="panel panel-default">
+        <div class="panel-body quote">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['quotes']
+}
+</script>
+
+<style lang="css">
+  .panel-body{
+    font-family: 'Arizonia', cursive;
+    font-size: 24px;
+    color: #6e6e6e;
+  }
+
+  .quote{
+    cursor: pointer;
+  }
+  .quote:hover{
+    background-color: #ffe2e2;
+  }
+</style>
