06439dfed7c35e73564ae4f0ed0aeabca3552237
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-16 14:48:06 -0500

    create a custom control

--- lessons/module_11/Section Code (Start)/src/App.vue
+++ lessons/module_11/Section Code (Start)/src/App.vue
@@ -101,6 +101,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <app-switch v-model="dataSwitch"></app-switch>
+              </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -127,9 +132,9 @@
                         <ul>
                             <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -138,6 +143,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
+
     export default {
       data () {
         return {
@@ -150,8 +157,12 @@
           sendMail: [],
           gender: 'Male',
           selectedPriority: 'High',
-          priorities: ['High', 'Medium', 'Low']
+          priorities: ['High', 'Medium', 'Low'],
+          dataSwitch: true
         }
+      },
+      components: {
+        appSwitch: Switch
       }
     }
 </script>
