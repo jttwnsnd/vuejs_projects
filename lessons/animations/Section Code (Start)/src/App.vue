5ce22b19b59c0c6186fcba743a7043709fed3e2c
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-27 12:37:22 -0500

    fade in and out

--- lessons/animations/Section Code (Start)/src/App.vue
+++ lessons/animations/Section Code (Start)/src/App.vue
@@ -3,6 +3,14 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
+                <hr>
+                <button type="button" class="btn btn-primary" name="button" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                  <div
+                    class="alert alert-info"
+                    v-if="show">This is some info.</div>
+                </transition>
 
             </div>
         </div>
@@ -13,12 +21,27 @@
     export default {
         data() {
             return {
-
+              show: false
             }
         }
     }
 </script>
 
 <style>
+  .fade-enter{
+    opacity: 0;
+  }
+  .fade-enter-active{
+    transition: opacity 1s;
+  }
 
+  .fade-leave{
+    /*default of opacity is 1*/
+  }
+
+  .fade-leave-active{
+    transition: opacity 1s;
+    /*default is 1, lets set it to 0*/
+    opacity: 0;
+  }
 </style>
