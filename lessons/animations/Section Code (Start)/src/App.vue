c5441b1c55120058b5f066d3002b43cfba90f34e
jttwnsnd <jt.a.townsend@gmail.com>
2017-01-30 14:33:17 -0500

    mini app project

--- lessons/animations/Section Code (Start)/src/App.vue
+++ lessons/animations/Section Code (Start)/src/App.vue
@@ -25,7 +25,21 @@
                 <hr>
                 <button type="button" name="button" class="btn btn-success">Toggle Components</button>
                 <br>
-                <component :is="selectedComponent"></component>
+                <transition name="fade" mode="out-in">
+                  <component :is="selectedComponent"></component>
+                </transition>
+                <hr>
+                <button type="button" name="button" class="btn btn-primary" @click="addItem">Add Item</button>
+                <br><br>
+                <ul class="list-group">
+                  <transition-group name="slide">
+                    <li class="list-group-item"
+                      v-for="(number, index) in numbers"
+                      @click="removeItem(index)"
+                      style="cursor: pointer"
+                      :key="number">{{ number }}</li>
+                  </transition-group>
+                </ul>
             </div>
         </div>
     </div>
@@ -41,12 +55,22 @@
               show: true,
               load: true,
               alertAnimation: 'fade',
-              selectedComponent: 'app-success-alert'
+              selectedComponent: 'app-success-alert',
+              numbers: [1, 2, 3, 4, 5]
             }
         },
         components: {
           appDangerAlert: DangerAlert,
           appSuccessAlert: SuccessAlert
+        },
+        methods: {
+          addItem(){
+            const pos = Math.floor(Math.random() * this.numbers.length);
+            this.numbers.splice(pos, 0, this.numbers.length + 1);
+          },
+          removeItem(index){
+            this.numbers.splice(index, 1);
+          }
         }
     }
 </script>
@@ -84,6 +108,10 @@
     animation: slide-out 1s ease-out forwards;
     transition: opacity 1s;
     opacity: 0;
+    position: absolute;
+  }
+  .slide-move{
+    transition: transform 1s;
   }
 
   @keyframes slide-in{
