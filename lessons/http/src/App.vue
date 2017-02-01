220a8a6773350536b09896c344f0f76a1e037f5b
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-01 09:31:54 -0500

    http request with vue-resource

--- lessons/http/src/App.vue
+++ lessons/http/src/App.vue
@@ -3,13 +3,59 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                  <label for="">Username</label>
+                  <input type="text" name="" value="" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label for="">Mail</label>
+                  <input type="text" name="" value="" class="form-control" v-model="user.email">
+                </div>
+                <button type="button" name="button" class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button type="button" name="button" @click="fetchData">Get Data</button>
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+    export default{
+      data(){
+        return{
+          user: {
+            username: '',
+            email: ''
+          },
+          users: []
+        }
+      },
+      methods: {
+        submit(){
+          this.$http.post('https://vuejs-http-b4077.firebaseio.com/data.json', this.user)
+            .then(response => {
+              console.log(response);
+            }, error=>{
+              console.log(error);
+            });
+        },
+        fetchData(){
+          this.$http.get('https://vuejs-http-b4077.firebaseio.com/data.json')
+            .then(response => {
+              return response.json();
+            })
+            .then(data => {
+              const resultArray = [];
+              for(let key in data){
+                resultArray.push(data[key]);
+              }
+              this.users = resultArray;
+            })
+        }
+      }
     }
 </script>
 
