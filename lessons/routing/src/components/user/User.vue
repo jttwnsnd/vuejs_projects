6dea89425921099ba187bbf3b0a08347630bfa8e
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-02 11:15:58 -0500

    sending back to router

--- lessons/routing/src/components/user/User.vue
+++ lessons/routing/src/components/user/User.vue
@@ -1,4 +1,16 @@
 <template>
+  <div class="">
     <h1>The User Page</h1>
+    <button type="button" name="button" class="btn btn-primary" @click="navigateToHome">Go To Home</button>
+  </div>
 
 </template>
+<script type="text/javascript">
+  export default {
+    methods: {
+      navigateToHome(){
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
