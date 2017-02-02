e3ee4cbb54d23ebd27f60ff36461650590c57080
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-02 16:22:35 -0500

    scrollbehavior only works in history mode. not hash.

--- lessons/routing/src/components/user/UserEdit.vue
+++ lessons/routing/src/components/user/UserEdit.vue
@@ -3,6 +3,8 @@
     <h3>Edit the User</h3>
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Query: {{ $route.query.q }}</p>
+    <div class="" style="height: 700px"></div>
+    <p id="data">Something down here</p>
   </div>
 
 </template>
--- lessons/routing/src/main.js
+++ lessons/routing/src/main.js
@@ -6,16 +6,7 @@ import { routes } from './routes'
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes,
-  scrollBehavior(to, from, savedPosition){
-    if(savedPosition){
-      return savedPosition;
-    }
-    if(to.hash){
-      return { selector: to.hash };
-    }
-    return { x:0, y:0 }
-  }
+  routes
 })
 
 new Vue({
