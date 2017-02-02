fa76f8cbe1867885c3f3eb5cbc1cd350e1f83fb8
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-02 11:41:09 -0500

    passing params on similar routes

--- lessons/routing/src/components/user/Another.vue
+++ lessons/routing/src/components/user/Another.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="">
-    <h1>Another Page</h1>
+    <h1>User {{ id }}</h1>
+    <p>Loaded ID: {{ id }}</p>
     <button type="button" name="button" class="btn btn-primary" @click="navigateToHome">Go To Home</button>
   </div>
 
 </template>
 <script type="text/javascript">
   export default {
+    data() {
+      return {
+        id: this.$route.params.id
+      }
+    },
+    watch: {
+      '$route'(to, from) {
+        this.id = to.params.id;
+      }
+    },
     methods: {
       navigateToHome(){
         this.$router.push('/');
