8670f56608025aa2a681569619290f5ac2432250
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-02 11:11:03 -0500

    routing, add transformation when changing

--- lessons/routing/src/App.vue
+++ lessons/routing/src/App.vue
@@ -3,15 +3,60 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
+                <hr>
+                <app-header></app-header>
+                <hr>
+                <transition name="slide" mode="out-in">
+                  <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+  import Header from './components/Header.vue'
     export default {
+      components: {
+        appHeader: Header
+      }
     }
 </script>
 
 <style>
+.slide-enter{
+
+}
+.slide-enter-active{
+  animation: slide-in 0.5s ease-out forwards;
+  transition: all 0.5s;
+}
+.slide-leave{
+
+}
+.slide-leave-active{
+  animation: slide-out 0.5s ease-out forwards;
+  transition: all 0.5s;
+}
+
+@keyframes slide-in {
+  from{
+    opacity: 0;
+    transform: translateX(-120px);
+  }
+  to{
+    opacity: 1;
+    transform: translateX(0px);
+  }
+}
+@keyframes slide-out {
+  from{
+    opacity: 1;
+    transform: translateX(0px);
+  }
+  to{
+    opacity: 0;
+    transform: translateX(-120px);
+  }
+}
 </style>
---
+++ lessons/routing/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <ul class="nav nav-pills">
+    <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+    <router-link to="/user" tag="li" active-class="active" exact><a>User</a></router-link>
+    <li role='presentation'><a href="http://www.jt-townsend.com/coffeeSite">JT Page</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
