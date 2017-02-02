c2ee661f76317e40f8d9875f6cdcb954256fe6a9
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-02 11:20:08 -0500

    dynamic routing

--- lessons/routing/src/components/Header.vue
+++ lessons/routing/src/components/Header.vue
@@ -2,6 +2,7 @@
   <ul class="nav nav-pills">
     <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
     <router-link to="/user" tag="li" active-class="active" exact><a>User</a></router-link>
+    <router-link to="/user/22" tag="li" active-class="active" exact><a>Another</a></router-link>
     <li role='presentation'><a href="http://www.jt-townsend.com/coffeeSite">JT Page</a></li>
   </ul>
 </template>
--- lessons/routing/src/routes.js
+++ lessons/routing/src/routes.js
@@ -1,7 +1,9 @@
 import User from './components/user/User.vue';
+import Another from './components/user/Another.vue';
 import Home from './components/Home.vue';
 
 export const routes = [
   { path: '', component: Home },
+  { path: '/user/:id', component: Another },
   { path: '/user', component: User }
 ];
