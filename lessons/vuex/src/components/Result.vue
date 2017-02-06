20d191384b3aaef8ca5fe179887121bfae7d2cc1
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-06 16:45:45 -0500

    getters

--- lessons/vuex/src/components/Result.vue
+++ lessons/vuex/src/components/Result.vue
@@ -1,12 +1,19 @@
 <template>
+  <div class="">
     <p>Counter is: {{ counter }}</p>
+    <p>Counter has had {{ clicks }}</p>
+  </div>
+
 </template>
 
 <script>
     export default {
         computed: {
           counter(){
-            return this.$store.state.counter;
+            return this.$store.getters.doubleCounters;
+          },
+          clicks(){
+            return this.$store.getters.stringCounter;
           }
         }
     }
