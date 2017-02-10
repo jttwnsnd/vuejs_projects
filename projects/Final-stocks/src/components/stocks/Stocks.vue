b1e46cf68277b0ebb967a87204499f211bbe3db5
jttwnsnd <jt.a.townsend@gmail.com>
2017-02-10 14:39:09 -0500

    adding vuex to project

--- projects/Final-stocks/src/components/stocks/Stocks.vue
+++ projects/Final-stocks/src/components/stocks/Stocks.vue
@@ -9,16 +9,6 @@ import Stock from './Stock.vue';
 export default {
   components: {
     appStock: Stock
-  },
-  data() {
-    return {
-      stocks: [
-        {id: 1, name: "BMW", price: 110},
-        {id: 2, name: "Google", price: 410},
-        {id: 3, name: "Apple", price: 210},
-        {id: 4, name: "HEMA", price: 50}
-      ]
-    }
   }
 }
 </script>
---
+++ projects/Final-stocks/src/data/stocks.js
@@ -0,0 +1,10 @@
+export default [
+  {id: 1, name: "BMW", price: 310},
+  {id: 2, name: "Google", price: 410},
+  {id: 3, name: "Apple", price: 210},
+  {id: 4, name: "HEMA", price: 50},
+  {id: 5, name: "MTG", price: 510},
+  {id: 6, name: "SQ ENIX", price: 460},
+  {id: 7, name: "Oreo", price: 340},
+  {id: 8, name: "Lays", price: 100}
+]
